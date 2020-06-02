fec6a8e3a30c2070bd80e2009c6c6eba441c00d0
debugtheworldbot <istiancz@gmail.com>
2020-06-02 15:31:08 +0800

    deploy

---
+++ precache-manifest.1e5ffe1890248053bdba8aad071adf57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1531d70fe2c89fd3e9d892e757bf31fb",
+    "url": "/morneyReactWeb/index.html"
+  },
+  {
+    "revision": "365320a19762ccd31401",
+    "url": "/morneyReactWeb/static/css/main.0f917ae3.chunk.css"
+  },
+  {
+    "revision": "4a3bbbe171e1cf7fb43c",
+    "url": "/morneyReactWeb/static/js/2.3cc68079.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/morneyReactWeb/static/js/2.3cc68079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365320a19762ccd31401",
+    "url": "/morneyReactWeb/static/js/main.6d4cc183.chunk.js"
+  },
+  {
+    "revision": "08704679804a7e5d3f20",
+    "url": "/morneyReactWeb/static/js/runtime-main.8d4cac5c.js"
+  }
+]);
